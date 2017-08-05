d00142eed35091a461ce996e5c3ac0b45900b68d
정지원 <4dakto@gmail.com>
2017-08-06 02:14:43 +0900

    Jiwon ::  [add] sort  in ascending order

--- sortArray.ts
+++ sortArray.ts
@@ -1,4 +1,28 @@
+// Sorting an array of strings
 const arr: ReadonlyArray<string> = ['foo', 'bar'];
 const copy = arr.slice().sort();
-console.log({ arr });
-console.log({ copy });
+
+// Sort in ascending order
+const movies = [
+  {
+    name: "The Shawshank Redemtion",
+    year: 1994
+  },
+  {
+    name: "The Godfather",
+    year: 1972
+  },
+  {
+    name: "The Godfather: Part II",
+    year: 1974
+  },
+  {
+    name: "The Dark Knight",
+    year: 2008
+  }
+];
+
+movies.sort((b, a) => b.year - a.year);
+for(let movie of movies) {
+  console.log(movie.name);
+}
